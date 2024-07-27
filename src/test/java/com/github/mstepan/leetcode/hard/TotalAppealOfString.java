29d21506c50657ac54d9f5be59d2e87d762149f2
mstepan <maksym.stepanenko@oracle.com>
2024-07-27 11:41:22 +0300

    2262. Total Appeal of A String. Leetcode solved.

---
+++ src/test/java/com/github/mstepan/leetcode/hard/TotalAppealOfString.java
@@ -0,0 +1,42 @@
+package com.github.mstepan.leetcode.hard;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * 2262. Total Appeal of A String
+ *
+ * <p>https://leetcode.com/problems/total-appeal-of-a-string/
+ */
+public class TotalAppealOfString {
+
+    /**
+     * N = str.length()
+     * K = ('z' - 'a' + 1) = 26
+     *
+     * time: O(N)
+     *
+     * space: O(K)
+     */
+    public long appealSum(String str) {
+        Objects.requireNonNull(str, "null 'str' detected");
+        long appealCount = 0;
+
+        int[] lastPos = new int['z' - 'a' + 1];
+        Arrays.fill(lastPos, -1);
+        long right = str.length() - 1;
+
+        for (int i = 0; i < str.length(); ++i) {
+            char ch = str.charAt(i);
+            assert ch >= 'a' && ch <= 'z';
+
+            long left = lastPos[ch - 'a'] + 1;
+
+            appealCount += (i - left + 1) * (right - i + 1);
+
+            lastPos[ch - 'a'] = i;
+        }
+
+        return appealCount;
+    }
+}
