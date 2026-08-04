907f1fe6f71978e329f9ee6451e33eb149a0cf30
Maksym Stepanenko <maksym.stepanenko@oracle.com>
2026-08-04 14:11:54 +0300

    460. LFU Cache template added.

---
+++ src/main/java/com/github/mstepan/leetcode/hard/LFUCache.java
@@ -0,0 +1,17 @@
+package com.github.mstepan.leetcode.hard;
+
+/**
+ * 460. LFU Cache
+ *
+ * <p>https://leetcode.com/problems/lfu-cache/description/
+ */
+public class LFUCache {
+
+    public LFUCache(int capacity) {}
+
+    public int get(int key) {
+        return -1;
+    }
+
+    public void put(int key, int value) {}
+}
